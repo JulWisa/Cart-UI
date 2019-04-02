dfba114cd7abd67186c3f74a0ad61c962c8a55fb
Julia <wisa@bk.ru>
2019-04-02 06:40:42 +0400

    Move router to file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
 import React, {Component} from 'react';
 import './App.css';
-import {BrowserRouter, Route, Switch} from "react-router-dom";
-import Layout from "./components/layouts/Layout";
 import {Provider} from "react-redux";
 import {store} from "./store";
-import CartContainer from "./components/containers/cart/CartContainer";
-import ProductsContainer from "./components/containers/products/ProductsContainer";
+import router from "./router";
 
 class App extends Component {
     render() {
         return (
             <div className="App">
                 <Provider store={store}>
-                    <BrowserRouter>
-                        <Layout>
-                            <Switch>
-                                <Route exact path="/" component={ProductsContainer}/>
-                                <Route exact path="/cart" component={CartContainer}/>
-                            </Switch>
-                        </Layout>
-                    </BrowserRouter>
+                    {router}
                 </Provider>
             </div>
         );
